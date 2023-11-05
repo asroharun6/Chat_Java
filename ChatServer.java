5fec58ee69f929b9884ef9e116777780d8228d4c
asro <129697106+asroharun6@users.noreply.github.com>
2023-11-05 23:45:12 +0700

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,12 +1,15 @@
 import java.io.*;
 import java.net.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatServer {
     private static final int PORT = 1234; // Port to listen on
     private static final String IP_ADDRESS = "192.168.70.191"; // Server IP address
-    private static Map<String, ClientHandler> clients = new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<String, ClientHandler> clients = new ConcurrentHashMap<>();
+    private static List<Message> chatHistory = Collections.synchronizedList(new ArrayList<>());
+    private static SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public static void main(String[] args) throws IOException {
         InetAddress bindAddr = InetAddress.getByName(IP_ADDRESS);
@@ -15,21 +18,24 @@ public class ChatServer {
 
             while (true) {
                 Socket socket = serverSocket.accept();
-                new Thread(new ClientHandler(socket)).start();
+                ClientHandler handler = new ClientHandler(socket);
+                new Thread(handler).start();
             }
         }
     }
 
-    // Broadcast a message to all clients except the sender
-    public static void broadcastMessage(String message, String senderName) {
+    private static void broadcastMessage(String message, String senderName) {
+        String timestamp = dateFormatter.format(new Date());
+        String formattedMessage = String.format("%s %s: %s", timestamp, senderName, message);
+        chatHistory.add(new Message(senderName, message, timestamp)); // Save to chat history
+
         for (ClientHandler client : clients.values()) {
             if (!client.getUserName().equals(senderName)) {
-                client.sendMessage(message);
+                client.sendMessage(formattedMessage);
             }
         }
     }
 
-    // ClientHandler class
     private static class ClientHandler implements Runnable {
         private Socket socket;
         private PrintWriter out;
@@ -56,23 +62,40 @@ public class ChatServer {
                 out.println("Enter your name: ");
                 userName = in.readLine();
                 clients.put(userName, this);
-                broadcastMessage("User " + userName + " has joined the chat.", userName);
+                broadcastMessage("has joined the chat", userName);
 
                 String input;
                 while ((input = in.readLine()) != null) {
-                    broadcastMessage(userName + ": " + input, userName);
+                    if ("quit".equalsIgnoreCase(input)) {
+                        break;
+                    }
+                    broadcastMessage(input, userName);
                 }
             } catch (IOException e) {
-                System.out.println("Error: " + e.getMessage());
+                System.out.println(userName + " Error: " + e.getMessage());
             } finally {
-                try {
+                if (userName != null) {
                     clients.remove(userName);
+                    broadcastMessage("has left the chat", userName);
+                }
+                try {
                     socket.close();
-                    broadcastMessage(userName + " has left the chat.", userName);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
+        }
+    }
+
+    private static class Message {
+        String user;
+        String message;
+        String timestamp;
+
+        public Message(String user, String message, String timestamp) {
+            this.user = user;
+            this.message = message;
+            this.timestamp = timestamp;
         }
     }
 }
